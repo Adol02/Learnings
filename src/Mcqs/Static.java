46678a27ad38c17687a855ece6ab658b6e21a292
Hareni <hareniraj2002@gmail.com>
2025-02-20 15:00:40 +0530

    Create Static.java

---
+++ src/Mcqs/Static.java
@@ -0,0 +1,16 @@
+package Mcqs;
+
+public class Static {
+	
+	public void temp() 
+	{ 
+	 float sum = 21; 
+	System.out.println(sum); 
+		//return(--(sum)); 
+	} 
+	public static void main(String[] args) 
+	{ 
+		Static test = new Static(); 
+		
+	} 
+}	 
