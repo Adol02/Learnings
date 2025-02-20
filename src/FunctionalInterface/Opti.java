6675c13dfa608a57c9a91bbf109ca44c7cead43b
Hareni <hareniraj2002@gmail.com>
2025-02-20 15:11:33 +0530

    Create Opti.java

---
+++ src/FunctionalInterface/Opti.java
@@ -0,0 +1,26 @@
+package trainingss;
+import java.util.Optional;
+public class Opti {
+      public static void main(String[] args) {
+    	String h =null;
+    	Optional<String> l = null;
+    	Optional<String> J =Optional.ofNullable(null);
+//    	System.out.println(h.toLowerCase());
+//    	System.out.println(h.startsWith("k"));
+//    	System.out.println(J.get().startsWith("k"));
+//    	System.out.println(J.get().equals("kil"));
+//    	System.out.println(h.startsWith("kil"));
+    	System.out.println(h.equals("kil"));
+    	
+    	
+//    	  if(h!=null) {
+//    		  h.toLowerCase();
+//    	  }
+//    	  else {
+//    		  System.out.println("hi");
+//    	  }
+//    	  if(J !=null) {
+//    	  }
+      }
+      
+}
