2d2946502303730f8dbf9a99ee05a0504738c32f
Hareni <hareniraj2002@gmail.com>
2025-02-20 14:55:37 +0530

    Create InheritEx.java

---
+++ src/Mcqs/InheritEx.java
@@ -0,0 +1,45 @@
+package Mcqs;
+
+//class One{	
+//	 static void print(){
+//		System.out.println("1");
+//	}
+//}
+//
+//class Two extends One{
+// public static void print(){
+//		System.out.println("2");
+//	}
+//}
+public class InheritEx {
+	
+	
+		public static void main(String args[]){
+//			One one = new Two();
+//			one.print();
+			 String invalidInt = "abc";
+			 int number = Integer.parseInt(invalidInt);
+			 System.out.println(number);
+			 Queue
+//			    try {
+//			        int number = Integer.parseInt(invalidInt);
+//			    } catch (NumberFormatException e) {
+//			        System.out.println("Caught NumberFormatException: " + e.getMessage());
+//			    }
+	}
+}
+//	public static void main(String[] args){	
+//		Parents parent = new Child();
+//		parent.className();
+//	}	
+//}
+//class Parents{
+//	public void className(){
+//		System.out.println("Parent");
+//	}
+//}
+//class Child extends Parents{
+//	void className(){
+//		System.out.println("Child");
+//	}
+//}
