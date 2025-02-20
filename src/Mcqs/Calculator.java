0367bf564b76827d0d82788a1566f40f95e1340d
Hareni <hareniraj2002@gmail.com>
2025-02-20 14:51:55 +0530

    Create Calculator.java

---
+++ src/Mcqs/Calculator.java
@@ -0,0 +1,18 @@
+package Mcqs;
+
+
+interface Add{
+	int sum(int a, int b);
+	
+}
+
+
+interface Sub extends Add{
+
+}
+
+public class Calculator {
+        public static void main(String[] args) {
+        	System.out.println("ji");
+        }
+}
