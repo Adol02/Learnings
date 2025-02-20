bb5783a3c6b0443085ba068fa807e1ebb8acf64f
Hareni <hareniraj2002@gmail.com>
2025-02-20 15:10:03 +0530

    Create Function.java

---
+++ src/FunctionalInterface/Function.java
@@ -0,0 +1,18 @@
+package FunctionalInterface;
+
+interface ERF{
+ 
+	static void aMethod() {
+		System.out.println("hi");
+	}
+	
+}
+public class Function implements ERF {
+	
+      public static void main(String[] args) {
+         ERF.aMethod();
+    	  ERF k = new Function();
+    	  StringBuilder
+    	  //k.aMethod();
+      }
+}
