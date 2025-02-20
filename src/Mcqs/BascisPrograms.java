fb41d573154503b902cf478c589c5ef6825ef103
Hareni <hareniraj2002@gmail.com>
2025-02-20 14:50:48 +0530

    Create BascisPrograms.java

---
+++ src/Mcqs/BascisPrograms.java
@@ -0,0 +1,31 @@
+package Mcqs;
+
+
+
+public class BascisPrograms {
+public static void main(String[] args) {
+	am(10);
+	final int a = 20;
+    String dif = "deepa";
+	dif ="this";
+	System.out.println(dif);
+	am(a);
+//	char c='d';
+//	System.out.println("hi"+a+j+k.length());
+//	double h=10/4;
+//	double g=1.0;
+//	double r=2.0;
+//	double k=g/r;
+//	System.out.println(h+","+k);	
+//	 System.out.println(1.0/0.0);
+//	 long u = 3l/0l;
+//	int f = 5;
+//	f = f++ + ++f+ f++;
+//	 System.out.println(f);
+}
+static void am(int a) {
+	a+=10;
+	System.out.println(a);
+}
+
+}
