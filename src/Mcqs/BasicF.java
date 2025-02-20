a1b5e03bacc4baf23698885f4183dfaa28ba8826
Hareni <hareniraj2002@gmail.com>
2025-02-20 14:51:16 +0530

    Create BasicF.java

---
+++ src/Mcqs/BasicF.java
@@ -0,0 +1,35 @@
+package Mcqs;
+
+public class BasicF {
+	
+//		// constructor 
+//		BasicF() 
+//		{ 
+//			System.out.println("Geeksforgeeks"); 
+//		} 
+//		BasicF a = new BasicF(); //line 8 
+	Throwable
+		public static void main(String args[]) 
+		{ 
+			BasicF b; //line 12 
+			b = new BasicF(); 
+			boolean f = true;
+			boolean s = false;
+			boolean w= (f==s);
+			short t = 20;
+//			int r = 5;
+//			r*=(2+8);
+			System.out.println(w);
+			 // \u000d System.out.println("Java comment is executed!!");  
+//			for(int p =7;p>3;p-=5) {
+//				System.out.println(p+++p);
+//			}
+			String h = "hello";
+//			boolean a = true;
+//			System.out.println(h.valueOf(a));
+//			
+//			h.concat("hi");
+			System.out.println(h.concat("hi"));
+			} 
+	} 
+
